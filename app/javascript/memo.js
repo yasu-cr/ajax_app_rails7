c977bb94551c2f1dd8ed8c85659050684885ba9e
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-11-22 16:57:11 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   //リクエストを送信する処理
   const form = document.getElementById("form");
@@ -8,7 +22,17 @@ function post (){
     XHR.open("POST", "/posts", true);     //第一引数にはHTTPメソッド、第二引数にはパス、第三引数には非同期通信であるかをtrueかfalse
     XHR.responseType = "json";     //（JavaScript Object Notation）
     XHR.send(formData);            //フォームに入力された内容をサーバー側に送信します.
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
- };
+};
  
- window.addEventListener('turbo:load', post);
+window.addEventListener('turbo:load', post);
